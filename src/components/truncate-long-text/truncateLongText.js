64d45d01707c20a80ac35613895ce7343e8bf5f1
Samuel Padgett <spadgett@redhat.com>
2017-03-26 13:51:51 -0400

    Add `truncate-long-text` directive and `truncate` filter

---
+++ src/components/truncate-long-text/truncateLongText.js
@@ -0,0 +1,33 @@
+'use strict';
+
+angular.module('openshiftCommonUI')
+  // Truncates text to a length, adding a tooltip and an ellipsis if truncated.
+  // Different than `text-overflow: ellipsis` because it allows for multiline text.
+  .directive('truncateLongText', function(truncateFilter) {
+    return {
+      restrict: 'E',
+      scope: {
+        content: '=',
+        limit: '=',
+        newlineLimit: '=',
+        useWordBoundary: '=',
+        expandable: '=',
+        keywords: '=highlightKeywords',  // optional keywords to highlight using the `highlightKeywords` filter
+        prettifyJson: '='                // prettifies JSON blobs when expanded, only used if expandable is true
+      },
+      templateUrl: 'src/components/truncate-long-text/truncateLongText.html',
+      link: function(scope) {
+        scope.toggles = {expanded: false};
+        scope.$watch('content', function(content) {
+          if (content) {
+            scope.truncatedContent = truncateFilter(content, scope.limit, scope.useWordBoundary, scope.newlineLimit);
+            scope.truncated = scope.truncatedContent.length !== content.length;
+          }
+          else {
+            scope.truncatedContent = null;
+            scope.truncated = false;
+          }
+        });
+      }
+    };
+  });
