634d70b78a944cca5e1d0e6f9f698d7ca983d059
Jeffrey Phillips <jephilli@redhat.com>
2017-05-15 13:42:16 -0400

    Remove extraneous note from binding application page.

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -21,7 +21,7 @@ angular.module('openshiftCommonUI').run(['$templateCache', function($templateCac
     "          <input type=\"radio\" ng-model=\"ctrl.serviceToBind\" value=\"\">\n" +
     "          Do not bind at this time.\n" +
     "        </label>\n" +
-    "        <div class=\"bind-description\">\n" +
+    "        <div ng-if=\"ctrl.allowNoBinding\" class=\"bind-description\">\n" +
     "          <span class=\"help-block service-instance-name\">\n" +
     "            You can create the binding later from your project.\n" +
     "          </span>\n" +
