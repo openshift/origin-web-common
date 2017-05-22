4918172973eb05f782481c7cc624cd9dcabe4bad
Robb Hamilton <rhamilto@redhat.com>
2017-05-22 16:11:12 -0400

    Migrating tileClick directive from console so it can also be used in catalog

--- dist/origin-web-common-ui.js
+++ dist/origin-web-common-ui.js
@@ -865,6 +865,23 @@ angular.module('openshiftCommonUI')
 ;'use strict';
 
 angular.module('openshiftCommonUI')
+  .directive('tileClick', function() {
+    return {
+      restrict: 'AC',
+      link: function($scope, element) {
+        $(element).click(function (evt) {
+          var t = $(evt.target);
+          if (t && t.closest("a", element).length) {
+            return;
+          }
+          $('a.tile-target', element).trigger("click");
+        });
+      }
+    };
+  });
+;'use strict';
+
+angular.module('openshiftCommonUI')
   .directive('toastNotifications', function(NotificationsService, $timeout) {
     return {
       restrict: 'E',
