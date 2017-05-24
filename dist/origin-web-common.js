0665e6d0e7795652407ed030b56800078f4fc0b4
Samuel Padgett <spadgett@redhat.com>
2017-05-24 07:39:16 -0400

    Don't trigger tile-click target if user clicks a button inside the tile

--- dist/origin-web-common.js
+++ dist/origin-web-common.js
@@ -1056,8 +1056,9 @@ angular.module('openshiftCommonUI')
       restrict: 'AC',
       link: function($scope, element) {
         $(element).click(function (evt) {
+          // Don't trigger tile target if the user clicked directly on a link or button inside the tile.
           var t = $(evt.target);
-          if (t && t.closest("a", element).length) {
+          if (t && (t.closest("a", element).length || t.closest("button", element).length)) {
             return;
           }
           $('a.tile-target', element).trigger("click");
