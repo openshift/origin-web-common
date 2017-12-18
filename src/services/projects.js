91506a695c32972d8a533f40bec3de2ceb50851a
David Taylor <dtaylor113@gmail.com>
2017-12-18 08:11:51 -0500

    Added 'opts.skipErrorNotFound' to ProjectsService.get

--- src/services/projects.js
+++ src/services/projects.js
@@ -42,7 +42,7 @@ angular.module('openshiftCommonServices')
       };
 
       return {
-        get: function(projectName) {
+        get: function(projectName, opts) {
           return  AuthService
                     .withUser()
                     .then(function() {
@@ -70,6 +70,9 @@ angular.module('openshiftCommonServices')
                                         });
                               }, function(e) {
                                 context.projectPromise.reject(e);
+                                if ((e.status === 403 || e.status === 404) && _.get(opts, 'skipErrorNotFound')) {
+                                  return $q.reject({notFound: true});
+                                }
                                 var description = 'The project could not be loaded.';
                                 var type = 'error';
                                 if(e.status === 403) {
