9a7f8745fd4090a3c10439bb94d8eee23dde91ac
Josef Karasek <jkarasek@redhat.com>
2018-08-29 13:43:37 +0200

    return value from authPromise

--- src/services/aggregatedLoggingService.js
+++ src/services/aggregatedLoggingService.js
@@ -3,72 +3,64 @@
 angular.module("openshiftCommonServices")
   .factory("AggregatedLoggingService", function($q, $cacheFactory, Logger, $interval, APIService, DataService) {
 
-    var currentUser = null;
     var cachedUserPermissions = $cacheFactory('operationsUsersCache', {
-          number: 10
-        });
+      number: 10
+    });
 
-    inFlightPermissionsRequests = {};
+    var inFlightPermissionsRequest = null;
+    var authPromise = null;
 
     // Create SelfSubjectAccessReview request againt authorization API to check whether
     // current user can view pods/log from 'openshift-logging' project.
     // Users who can view such logs are 'operations' users
-    var fetchUserPermisions = function(userName) {
-      var deferred = $q.defer();
-      currentUser = userName;
+    var isOperationsUser = function(userName) {
       var userAllowed = cachedUserPermissions.get(userName);
 
       if (!userAllowed) {
-          // If a request is already in flight, return the promise for that request.
-          if (inFlightPermissionsRequests[userName]) {
-            return inFlightPermissionsRequests[userName];
-          }
+        // If a request is already in flight, return the promise for that request.
+        if (inFlightPermissionsRequest) {
+          return inFlightPermissionsRequest;
+        }
 
-          Logger.log("AggregatedLoggingService, loading whether user " + userName + " is Operations user");
-          inFlightPermissionsRequests[userName] = deferred.promise;
-          var ssar = {
-            apiVersion: 'authorization.k8s.io/v1',
-            kind: 'SelfSubjectAccessReview',
-            spec: {
-              resourceAttributes: {
-                resource: 'pods/log',
-                namespace: 'openshift-logging',
-                verb: 'view'
-              }
+        Logger.log("AggregatedLoggingService, loading whether user " + userName + " is Operations user");
+        var ssar = {
+          apiVersion: 'authorization.k8s.io/v1',
+          kind: 'SelfSubjectAccessReview',
+          spec: {
+            resourceAttributes: {
+              resource: 'pods/log',
+              namespace: 'openshift-logging',
+              verb: 'view'
             }
-          };
-          DataService.create({ group: 'authorization.k8s.io', version: 'v1', resource: 'selfsubjectaccessreviews'},
-                            null, ssar, {namespace: 'openshift-logging'}).then(
-            function(data) {
-              var isAllowed = data.status.allowed;
-              cachedUserPermissions.put(userName, {'allowed': isAllowed,
-                                                    forceRefresh: false,
-                                                    cacheTimestamp: _.now()
-                                                  });
-              deferred.resolve();
-            }, function() {
-              deferred.resolve();
-          }).finally(function() {
-            delete inFlightPermissionsRequests[userName];
-          });
-
+          }
+        };
+        authPromise = DataService.create({ group: 'authorization.k8s.io', version: 'v1', resource: 'selfsubjectaccessreviews'},
+                          null, ssar, {namespace: 'openshift-logging'}).then(
+          function(data) {
+            var isAllowed = data.status.allowed;
+            cachedUserPermissions.put(userName, {'allowed': isAllowed,
+                                                  forceRefresh: false,
+                                                  cacheTimestamp: _.now()
+                                                });
+            return Promise.resolve(isAllowed);
+          }, function() {
+            return Promise.resolve(false);
+        }).finally(function() {
+          inFlightPermissionsRequest = null;
+        });
+        inFlightPermissionsRequest = authPromise;
       } else {
         // Using cached data.
         Logger.log("AggregatedLoggingService, using cached user " + userName);
         if ((_.now() - userAllowed.cacheTimestamp) >= 600000) {
           userAllowed.forceRefresh = true;
         }
-        deferred.resolve();
+        return $q.when(!!_.get(userAllowed, ['allowed']));
       }
-      return deferred.promise;
-    };
-
-    var isOperationsUser = function(userName) {
-      return !!_.get(cachedUserPermissions.get(userName || currentUser), ['allowed']);
+      return authPromise;
     };
 
     return {
-      fetchUserPermisions: fetchUserPermisions,
       isOperationsUser: isOperationsUser
     };
   });
